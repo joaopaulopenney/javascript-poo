b1da88578ea00b833c4de114102ca691fece676c
João Paulo Penney <137862398+joaopaulopenney@users.noreply.github.com>
2023-09-05 04:36:12 -0300

    Aulas 03 e 04

---
+++ Aula03/app.js
@@ -0,0 +1,31 @@
+var ingredientes = ['mel', 'água', 'sal', 'mostarda'];
+
+
+function capitalizar(ingredientes) {
+    var modificado = [];
+
+    for (var i = 0; i < ingredientes.length; i++) {
+        var letraInicial = ingredientes[i].charAt(0).toUpperCase();
+        var restoTexto = ingredientes[i].slice(1);
+        var resultado = letraInicial + restoTexto;
+    
+        modificado[i] = resultado;
+    }
+
+    return modificado;
+}
+
+function ordenar(ingredientes) {
+    return ingredientes.sort(function(a, b) {
+        return a.localeCompare(b);
+    });
+}
+
+var resultadoCapitalizado = capitalizar(ingredientes);
+var resultadoOrdenacao = ordenar(resultadoCapitalizado)
+console.log(resultadoCapitalizado);
+console.log(resultadoOrdenacao);
+
+
+
+
---
+++ Aula04/app.js
@@ -0,0 +1,13 @@
+import modificador from './modificador.js'
+
+console.log(modificador)
+
+var ingredientes = ['mel', 'água', 'sal', 'mostarda'];
+var resultadoCapitalizado = modificador.capitalizar(ingredientes);
+var resultadoOrdenacao = modificador.ordenar(resultadoCapitalizado)
+var resultadoCaixaAlta = modificador.caixaAlta(ingredientes)
+
+
+console.log(resultadoCapitalizado);
+console.log(resultadoOrdenacao);
+console.log(resultadoCaixaAlta);
