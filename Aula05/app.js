4e0f6c4c87139ca142356aceac3d26abbbdb56d9
João Paulo Penney <137862398+joaopaulopenney@users.noreply.github.com>
2023-09-06 04:22:09 -0300

    Aulas 05 e 06

---
+++ Aula05/app.js
@@ -0,0 +1,20 @@
+import modificador from './modificador.js'
+import ingredientes from './ingredientes.js';
+
+console.log(ingredientes);
+console.log(typeof ingredientes);
+console.log(Array.isArray(ingredientes));
+
+console.log(ingredientes[0].nome)
+
+
+/*var ingredientes = ['mel', 'água', 'sal', 'mostarda'];
+var resultadoCapitalizado = modificador.capitalizar(ingredientes);
+var resultadoOrdenacao = modificador.ordenar(resultadoCapitalizado)
+var resultadoCaixaAlta = modificador.caixaAlta(ingredientes)
+
+
+console.log(resultadoCapitalizado);
+console.log(resultadoOrdenacao);
+console.log(resultadoCaixaAlta);
+*/
