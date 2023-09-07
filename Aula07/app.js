5962a08e4657ddbcdfc1c865c2d3e4cdc8a2105b
João Paulo Penney <137862398+joaopaulopenney@users.noreply.github.com>
2023-09-07 05:33:37 -0300

    Aula 07

---
+++ Aula07/app.js
@@ -0,0 +1,25 @@
+import modificador from './modificador.js'
+import ingredientes from './ingredientes.js';
+
+let ingredientesOrdenados = modificador.ordenar(ingredientes, 'nome');
+let ingredientesCapitalizados = modificador.capitalizar(ingredientesOrdenados, 'nome')
+let containerIngredientes = document.getElementById('container-ingredientes');
+
+for(let ingrediente of ingredientesCapitalizados) {
+    let textoHTML = `
+    <div class="ingrediente">
+        <img src="img/${ingrediente.img}">
+        <p class="nome-ingrediente">${ingrediente.nome}</p>    
+    </div>
+    `;
+
+    containerIngredientes.innerHTML += textoHTML;
+}
+
+
+
+
+/* <div class="ingrediente">
+        <img src="img/agriao.png">
+        <p class="nome-ingrediente">Agrião</p>    
+   </div> */
